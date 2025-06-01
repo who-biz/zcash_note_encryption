96207730586d85bd02aa9199dbf32cebb24bc7a5
who-biz <37732338+who-biz@users.noreply.github.com>
2025-06-01 15:35:53 -0400

    Change arguments in ka_derive_public_from_recipient

--- src/lib.rs
+++ src/lib.rs
@@ -163,7 +163,7 @@ pub trait Domain {
     /// with no required knowledge of Note data.
     fn ka_derive_public_from_recipient(
         recipient: &Self::Recipient,
-        esk_bytes: &EphemeralKeyBytes,
+        esk: &Self::EphemeralSecretKey,
     ) -> Self::EphemeralPublicKey;
 
     /// Derives `EphemeralPublicKey` from `esk` and the note's diversifier.
